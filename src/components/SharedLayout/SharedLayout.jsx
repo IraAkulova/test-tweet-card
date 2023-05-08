1169b4da1f51047d01803341c3eba5b1d6dfe569
Ira Akulova <ira.akulova2019@gmail.com>
2023-05-08 17:14:27 +0300

    fixed footer position

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -5,7 +5,7 @@ import Loader from "../Loader/Loader";
 
 const SharedLayout = () => {
   return (
-    <div>
+    <div className={css.layout}>
       <header>
         <nav className={css.navigation}>
           <NavLink to="/" className={css.link}>
@@ -19,6 +19,21 @@ const SharedLayout = () => {
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
+      <footer
+        style={{
+          display: "flex",
+          // gap: 30,
+          alignItems: "center",
+          paddingLeft: 50,
+          height: 50,
+          background:
+            "linear-gradient(114.99deg, #471CA9 -0.99%, #5736A3 54.28%, #4B2A99 78.99%)",
+          boxShadow: "-2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23)",
+          fontSize: "large",
+          fontWeight: 700,
+          color: "#EBD8FF",
+        }}
+      ></footer>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 const Home = () => {
     return (
-      <>
         <main>
           <p
             style={{
@@ -9,7 +8,7 @@ const Home = () => {
               fontStyle: "normal",
               fontWeight: 500,
               fontSize: 20,
-              lineHeight: 24,
+              lineHeight: 26,
               textTransform: "uppercase",
               textAlign: "center",
             }}
@@ -17,20 +16,7 @@ const Home = () => {
             Welcome page
           </p>
         </main>
-            {/* <footer style={{
-    display: 'flex',
-    gap: 30,
-    alignItems: 'center',
-    paddingLeft: 50,
-    height: 50,
-    background: 'linear-gradient(114.99deg, #471CA9 -0.99%, #5736A3 54.28%, #4B2A99 78.99%)',
-    boxShadow: '-2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23)',
-    fontSize: 'large',
-    fontWeight: 700,
-    color: '#EBD8FF',
 
-        }}></footer> */}
-      </>
     );
 };
 
