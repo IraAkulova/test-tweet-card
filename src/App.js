930ec7a0ace14a7d5727ab51b98acc2f566e9a52
Ira Akulova <ira.akulova2019@gmail.com>
2023-05-08 12:17:33 +0300

    added sharedlayout and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import SharedLayout from './components/SharedLayout/SharedLayout';
+import Home from './pages/Home';
+import Tweets from './pages/Tweets';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="tweets" element={<Tweets />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,24 @@
+import { Outlet, NavLink } from "react-router-dom";
+import { Suspense } from "react";
+import css from "./SharedLayout.module.css";
+import Loader from "../Loader/Loader";
+
+const SharedLayout = () => {
+  return (
+    <div>
+      <nav className={css.navigation}>
+        <NavLink to="/" className={css.link}>
+          Home
+        </NavLink>
+        <NavLink to="/tweets" className={css.link}>
+          Tweets
+        </NavLink>
+      </nav>
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
+};
+
+export default SharedLayout;
