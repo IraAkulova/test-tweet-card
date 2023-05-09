590ccf328678480f6d9c574c16ffdd35db8fa5e9
Ira Akulova <ira.akulova2019@gmail.com>
2023-05-09 15:49:01 +0300

    testing vercel

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -20,18 +20,9 @@ const SharedLayout = () => {
         <Outlet />
       </Suspense>
       <footer
+        className={css.footer}
         style={{
-          display: "flex",
-          // gap: 30,
-          alignItems: "center",
-          paddingLeft: 50,
-          height: 50,
-          background:
-            "linear-gradient(114.99deg, #471CA9 -0.99%, #5736A3 54.28%, #4B2A99 78.99%)",
-          boxShadow: "-2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23)",
-          fontSize: "large",
-          fontWeight: 700,
-          color: "#EBD8FF",
+
         }}
       ></footer>
     </div>
