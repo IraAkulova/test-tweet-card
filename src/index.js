2e36673889c29c9047fe970d3ea7e5314aa44c46
Ira Akulova <ira.akulova2019@gmail.com>
2023-05-09 15:44:58 +0300

    testing vercel

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <BrowserRouter basename="/test-tweet-card">
-        <App />
+    <BrowserRouter basename="/test-tweet-card">
+      <h1>Hello</h1>
+        {/* <App /> */}
       </BrowserRouter>
   </React.StrictMode>
 );
